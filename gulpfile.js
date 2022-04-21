48645c42ebb232856080fe8e82c79ae88a7f3869
Vardan Khachatryan <khachatryan_va@mail.ru>
2022-04-21 15:34:16 +0300

    gulpfile+package

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-const {series, parallel, task, dest, src} = require('gulp');
+const {series, watch, parallel, task, dest, src} = require('gulp');
 const clean = require('gulp-clean');
 const cleanCSS = require('gulp-clean-css');
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 task('clean', function () {
     return src('./public/*', {read: false})
@@ -26,6 +28,7 @@ task('scripts', function() {
 task('scss', function() {
     return src('./src/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(cleanCSS())
         .pipe(dest('./public/styles/'));
 
@@ -37,5 +40,14 @@ task('pug', function() {
         .pipe(dest('./public/'));
 });
 
+const build = series('clean', parallel('scripts', 'scss', 'pug'))
 
-exports.default=series('clean', parallel('scripts', 'scss', 'pug'));
+task('dev', function() {
+    browserSync.init({
+        server: "./public"
+    });
+    watch("src/**/*.pug", build).on("change", browserSync.reload)
+});
+
+exports.build= build;
+exports.dev=series(build, "dev");
