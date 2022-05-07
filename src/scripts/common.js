43fc7e47b0be2aef2b0833333ae69e7961c0524b
Vardan Khachatryan <khachatryan_va@mail.ru>
2022-05-07 17:49:30 +0300

    scroll+slider(glider)

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -30,8 +30,8 @@
                <path d="M13.0607 0.939577C12.4749 0.353795 11.5252 0.353801 10.9394 0.939592L1.3935 10.4856C0.807718 11.0714 0.807724 12.0211 1.39351 12.6069C1.9793 13.1927 2.92905 13.1927 3.51483 12.6069L12.0001 4.12156L20.4854 12.6068C21.0712 13.1926 22.0209 13.1926 22.6067 12.6068C23.1925 12.021 23.1925 11.0712 22.6067 10.4855L13.0607 0.939577ZM13.5001 4.0001L13.5 2.00023L10.5 2.00025L10.5001 4.00012L13.5001 4.0001Z" fill="#222021"/>
             </svg>
             `
-        scrollBackButton.addEventListener("click", function (){
-            window.scrollTo(0,0)
+        scrollBackButton.addEventListener("click", function () {
+            window.scrollTo(0, 0)
         })
         return scrollBackButton;
     }
@@ -54,8 +54,4 @@
             scrollBackButton.remove()
         }
     })
-
-
-
-
 }
