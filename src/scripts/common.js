0b0c99557faf2e1ceb1c82b97e1d36b32c6e973e
Vardan Khachatryan <khachatryan_va@mail.ru>
2022-05-14 22:26:25 +0300

    refactored nav and scroll logic

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,28 +1,4 @@
-document.querySelector(".restaurant-menu__item").classList.add("restaurant-menu__item_active");
-
-let lastScrolledAreaId;
-
-function menuScroll() {
-    let areaElements = document.getElementById("menuList").children;
-
-    for (let i = 0; i < areaElements.length; i++) {
-        if (areaElements[i].getBoundingClientRect().top <= 78) {
-            lastScrolledAreaId = areaElements[i].getAttribute("id");
-        }
-    }
-
-    document.querySelector(".restaurant-menu__item_active")?.classList.remove("restaurant-menu__item_active");
-
-    let menuListElements = document.getElementsByClassName("restaurant-menu__item");
-    Array.from(menuListElements).find(function (li) {
-        if (li.children[0].getAttribute("href") === "#" + lastScrolledAreaId) {
-            li.classList.add("restaurant-menu__item_active");
-        }
-    })
-    if (areaElements[areaElements.length - 1].getBoundingClientRect().bottom <= 78) {
-        document.querySelector(".restaurant-menu__item_active")?.classList.remove("restaurant-menu__item_active");
-    }
-}
+const NAVIGATION_HEIGHT = 78;
 
 function createScrollBackButton() {
     let scrollBackButton = document.createElement('button');
@@ -32,31 +8,73 @@ function createScrollBackButton() {
                <path d="M13.0607 0.939577C12.4749 0.353795 11.5252 0.353801 10.9394 0.939592L1.3935 10.4856C0.807718 11.0714 0.807724 12.0211 1.39351 12.6069C1.9793 13.1927 2.92905 13.1927 3.51483 12.6069L12.0001 4.12156L20.4854 12.6068C21.0712 13.1926 22.0209 13.1926 22.6067 12.6068C23.1925 12.021 23.1925 11.0712 22.6067 10.4855L13.0607 0.939577ZM13.5001 4.0001L13.5 2.00023L10.5 2.00025L10.5001 4.00012L13.5001 4.0001Z" fill="#222021"/>
             </svg>
             `
-    scrollBackButton.addEventListener("click", function () {
-        window.scrollTo(0, 0)
-    })
+
+    function scrollToTop() {
+        if (scrollY > 0) {
+            window.scrollBy(0, -100);
+            setTimeout(scrollToTop, 20);
+        }
+    }
+
+    scrollBackButton.addEventListener("click", scrollToTop)
     return scrollBackButton;
 }
 
 let scrollBackButton = createScrollBackButton();
+document.querySelector("body").append(scrollBackButton);
+
+let navLinks = document.querySelectorAll(".restaurant-menu__link");
+
+function getPosition(element) {
+    return document.documentElement.scrollTop + element.getBoundingClientRect().y;
+}
+
+function scrollToTarget(event) {
+    let targetId = event.target.getAttribute("to");
+    let target = document.getElementById(targetId);
+    let targetPosition = getPosition(target);
+    let offsetTargetPosition = targetPosition - NAVIGATION_HEIGHT;
+
+    window.scrollTo({
+        top: offsetTargetPosition,
+        behavior: "smooth",
+    })
+}
+
+for (const navLink of navLinks) {
+    navLink.addEventListener("click", scrollToTarget);
+}
+
+let sections = document.querySelectorAll(".menu-list__item");
+
+function changeNavActiveItem() {
+    for (let i = 0; i < sections.length; i++) {
+        let {y: sectionY, height: sectionHeight} = sections[i].getBoundingClientRect();
+        let li = document.querySelector(`[to="${sections[i].id}"]`).parentNode
+
+        if (sectionY <= NAVIGATION_HEIGHT && sectionY > NAVIGATION_HEIGHT - sectionHeight - 52) {
+            li.classList.add("restaurant-menu__item_active");
+        } else {
+            li.classList.remove("restaurant-menu__item_active");
+        }
+    }
+}
 
 window.addEventListener("scroll", function () {
+    changeNavActiveItem();
     let headerBottomSection = document.querySelector(".header__bottom-section");
     if (scrollY >= 583) {
         headerBottomSection.classList.add("header__bottom-section_fixed");
         headerBottomSection.style.width = document.querySelector(".wrapper").clientWidth - 160 + 'px';
-        menuScroll()
-
-        document.querySelector("body").append(scrollBackButton)
-
+        scrollBackButton.style.opacity = "1";
     } else {
         headerBottomSection.classList.remove("header__bottom-section_fixed");
         headerBottomSection.style.width = "auto";
+        scrollBackButton.style.opacity = "0";
 
-        scrollBackButton.remove()
     }
 })
 
-document.querySelector(".back-button")?.addEventListener("click", function (){
+document.querySelector(".back-button")?.addEventListener("click", function () {
     history.back();
 })
